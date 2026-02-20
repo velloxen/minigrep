46bce23c23a9df4e322e538936c5f6e6e391ceeb
blob1234567890 <ben.k.olson@gmail.com>
2026-02-21 00:00:36 +0900

    technically working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+// `main` defines what happens when the end executable is run
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+    let pattern = &args[1];
+    let file_name = &args[2];
+
+    let file_contents = std::fs::read_to_string(file_name).unwrap();
+
+    let lines = file_contents.lines();
+    let res = lines.filter(|l| l.contains(pattern));
+
+    let res_string = itertools::join(res, "\n");
+    println!("{}", res_string);
 }
